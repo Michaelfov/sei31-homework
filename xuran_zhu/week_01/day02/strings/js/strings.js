b93640f91679b428b938d296cc1d5232d1bb41f8
MichaelZhu <michaelzhu@zhangyijiedeMacBook-Pro.local>
2019-04-08 13:57:43 +1000

    updated some work

--- xuran_zhu/week_01/day02/strings/js/strings.js
+++ xuran_zhu/week_01/day02/strings/js/strings.js
@@ -80,11 +80,11 @@
 // }
 // console.log (fixStart("babble"));
 // // Verbing
-Create a function called verbing. It should take a single argument, a string. If its length is at least 3, it should add 'ing' to its end, unless it already ends in 'ing', in which case it should add 'ly' instead. If the string length is less than 3, it should leave it unchanged. For example:
+// Create a function called verbing. It should take a single argument, a string. If its length is at least 3, it should add 'ing' to its end, unless it already ends in 'ing', in which case it should add 'ly' instead. If the string length is less than 3, it should leave it unchanged. For example:
 
-  verbing('swim'): 'swimming'
-  verbing('swimming'): 'swimmingly'
-  verbing('go'): 'go'
+  // verbing('swim'): 'swimming'
+  // verbing('swimming'): 'swimmingly'
+  // verbing('go'): 'go'
 
 // const verbing = function(verb) {
 //   for(let i = 0; i < verb.length; i++) {
@@ -101,41 +101,30 @@ Create a function called verbing. It should take a single argument, a string. If
 //
 //
 //
-Not Bad
-Create a function called notBad that takes a single argument, a string.
+// Not Bad
+// Create a function called notBad that takes a single argument, a string.
+//
+// It should find the first appearance of the substring 'not' and 'bad'.
+// If the 'bad' follows the 'not', then it should replace the whole 'not'...'bad' substring with 'good' and return the result.
+// If it doesn't find 'not' and 'bad' in the right sequence (or at all), just return the original sentence.
+// For example:
 
-It should find the first appearance of the substring 'not' and 'bad'.
-If the 'bad' follows the 'not', then it should replace the whole 'not'...'bad' substring with 'good' and return the result.
-If it doesn't find 'not' and 'bad' in the right sequence (or at all), just return the original sentence.
-For example:
-
-  notBad('This dinner is not that bad!'): 'This dinner is good!'
-  notBad('This movie is not so bad!'): 'This movie is good!'
-  notBad('This dinner is bad!'): 'This dinner is bad!'
+// notBad('This dinner is not that bad!'): 'This dinner is good!'
+// notBad('This movie is not so bad!'): 'This movie is good!'
+// notBad('This dinner is bad!'): 'This dinner is bad!'
 
 const notBad = function(str) {
-  const strToArray = str.split('');
-  for(let i=0; i)
-  for (let i = 0; i < str.length; i++) {
+  // for (let i = 0; i < str.length; i++) {
     if (str.includes("not") && str.includes("bad") && str.indexOf("not") < str.indexOf("bad")) {
-
-
+       let newStr = str.slice(0, str.indexOf("not"))
+       let afterBad = str.slice(str.indexOf("bad" + 3), str.length)
+       newStr += "good" + afterBad
+       console.log(str);
+       console.log(newStr);
     } else {
       console.log(str);
     }
-  }
+  // }
 }
 
-const test = [1, 2, 3];
-const doubleNumInArray = value =>{
-  let result = [];
-
-
-  for(let i=0; i<value.length; i++) {
-    result[i] = value[i]*2;
-
-  }
-  return result;
-}
-f
 console.log(notBad("This movie is not that bad."));
---
+++ xuran_zhu/week_02/ga-bank/js/account.js
@@ -0,0 +1,18 @@
+const accounts = {
+  checkingBalance: 0,
+  savingsBalance: 0,
+
+
+
+total: function() {
+    return this.checkingBalance + this.savingsBalance;
+},
+
+
+  checkingDeposit: function(amonut) {
+    this.checkingBalance += Number(amount);
+  }
+
+
+
+};
